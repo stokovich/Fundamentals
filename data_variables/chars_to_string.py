ff4ecf9b7828d24f6917a70ba95c1b3527353ea8
Stoka <spenchev@gmail.com>
2024-01-16 08:40:49 +0200

    with additional tasks made

---
+++ data_variables/chars_to_string.py
@@ -0,0 +1,7 @@
+char1 = input()
+char2 = input()
+char3 = input()
+
+new_string = char1 + char2 + char3
+
+print(new_string)
---
+++ data_variables/integer_operations.py
@@ -0,0 +1,12 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+
+first_operation = num1 + num2
+
+second_operation = first_operation // num3
+
+final_result = second_operation * num4
+
+print(final_result)
